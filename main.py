b60be4c28a258afa2640daf40465f39c2a6851ad
Q23RF <97279294+Q23RF@users.noreply.github.com>
2022-10-25 05:12:10 +0000

    line notify init + 2 buttons styled

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 from poet import write
 from mail import send
-from generate import write_on_img
-from werkzeug.utils import secure_filename
+from post import write_on_img, send_line
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def main():
     if request.method == "POST":
-        if int(request.form["id"])==2:
-
-            print("generating img...")
-            p = request.form["poem"]
-            poem_list = p[2:-2].split("', '")
-            ptext=""
-            for line in poem_list:
-                ptext += line[:-2] + '\n'
-                print(line)
-            re = write_on_img(ptext).save("data/pc.png")
-            return send_from_directory('data', "pc.png", as_attachment=True)
-
-
-        else:
+        if int(request.form["id"])==1:	#寫詩
             n = int(request.form["n"])
             len = int(request.form["len"])
             style = int(request.form["style"])
             re = write(n, len, style)
             return render_template('main.html', poem=re)
+        else:	#下載
+            p = request.form["poem"]
+            poem_list = p[2:-2].split("', '")
+            ptext=""
+            for line in poem_list:
+                line = line.replace("\\n", "\n")
+                ptext += line
+            if int(request.form["id"])==3:	#要影印
+                send_line(ptext)
+            re = write_on_img(ptext).save("data/pc.png")
+            return send_from_directory('data', "pc.png", as_attachment=True)
 
-
-    else:
-        return render_template('main.html', poem='')
+    return render_template('main.html', poem='')
 
 
 @app.route('/feedback', methods=["POST", "GET"])
--- post.py
+++ post.py
@@ -1,10 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
-from flask import url_for
+import requests
 
 def write_on_img(message):
   #img = Image.open("{{ url_for('static', filename='2.jpg') }}")
   img = Image.open("static/2.jpg")
-  font = ImageFont.truetype('static/莫大毛筆-Regular.ttf', 90)
+  font = ImageFont.truetype('static/莫大毛筆-Regular.ttf', 80)
   #font = ImageFont.load_default()
   imgDraw = ImageDraw.Draw(img)
 
@@ -14,3 +14,10 @@ def write_on_img(message):
 
   imgDraw.multiline_text((xText, yText), message, font=font, fill=(50, 50, 50), spacing=35)
   return img
+
+def send_line(message):
+	token = 'lKBD5xj141Gv6zZzYt6ycefuKRmfstxgfWRPKoxP74S'
+	headers = { "Authorization": "Bearer " + token }
+	data = { 'message': '\n'+message }
+	requests.post("https://notify-api.line.me/api/notify",
+	    headers = headers, data = data)
